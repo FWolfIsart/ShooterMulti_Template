e332487531fffb06bcbd58cf5240e789ec7afd7d
FWolfIsart <56432342+FWolfIsart@users.noreply.github.com>
2022-11-23 09:43:08 +0100

    [Add] project sources and config files

---
+++ Source/ShooterMulti/LD/PickupDirector.cpp
@@ -0,0 +1,83 @@
+#include "PickupDirector.h"
+#include "Engine/World.h"
+#include "../GameFramework/DeathMatchGS.h"
+
+APickupDirector* APickupDirector::Instance = nullptr;
+
+APickupDirector::APickupDirector()
+{
+}
+
+void APickupDirector::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (!Instance)
+		Instance = this;
+
+	IsSpawnFullArray.SetNum(SpawnPoints.Num());
+	
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &APickupDirector::SpawnTick, SecondPerSpawn, true);
+
+	ADeathMatchGS* GameState = Cast<ADeathMatchGS>(GetWorld()->GetGameState());
+	GameState->OnPlayerNum.AddLambda([this](ADeathMatchGS* GS) { UpdateFrequencies(GS); });
+}
+
+void APickupDirector::SpawnTick()
+{
+	if (bIsFull)
+		return;
+	
+	int MaxPoints = SpawnPoints.Num() - 1;
+	int RandomPoint = FMath::RandRange(0, MaxPoints);
+	int PrevPoint = RandomPoint;
+
+	while (IsSpawnFullArray[RandomPoint])
+	{
+		RandomPoint = (RandomPoint + 1) % MaxPoints;
+		if (RandomPoint == PrevPoint)
+		{
+			bIsFull = true;
+			return;
+		}
+	}
+
+	IsSpawnFullArray[RandomPoint] = true;
+	SpawnPickup(CurrentPickupIndex, RandomPoint);
+	CurrentPickupIndex = (CurrentPickupIndex + 1) % PickupBPs.Num();
+}
+
+void APickupDirector::SpawnPickup(int pickupIndex, int spawnPointIndex)
+{
+	auto pickupBP = PickupBPs[pickupIndex];
+	auto pickupLocation = SpawnPoints[spawnPointIndex]->GetActorLocation();
+	auto pickupRotation = SpawnPoints[spawnPointIndex]->GetActorRotation();
+
+	auto Pickup = GetWorld()->SpawnActor<APickup>(pickupBP, pickupLocation, pickupRotation);
+
+	if (Pickup)
+	{
+		Pickup->SpawnKey.ClassKey = pickupIndex;
+		Pickup->SpawnKey.SpawnPointKey = spawnPointIndex;
+	}
+}
+
+void APickupDirector::FreePickup(FSpawnKey Key)
+{
+	if (!Instance)
+		return;
+
+	Instance->IsSpawnFullArray[Key.SpawnPointKey] = false;
+}
+
+void APickupDirector::SetFull(bool isFull)
+{
+	bIsFull = isFull;
+}
+
+void APickupDirector::UpdateFrequencies(ADeathMatchGS* GameState)
+{
+	bIsFull = false;
+
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &APickupDirector::SpawnTick, GameState->NewFrequency(SecondPerSpawn), true);
+}
---
+++ Source/ShooterMulti/LD/PickupDirector.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "Pickup.h"
+#include "GameFramework/Actor.h"
+#include "PickupDirector.generated.h"
+
+UCLASS()
+class SHOOTERMULTI_API APickupDirector : public AActor
+{
+	GENERATED_BODY()
+	
+private:
+
+	static APickupDirector* Instance;
+
+	TArray<bool> IsSpawnFullArray;
+	int CurrentPickupIndex = 0;
+
+	FTimerHandle TimerHandle;
+
+	bool bIsFull = false;
+
+protected:
+
+	virtual void BeginPlay() override;
+
+public:
+
+	APickupDirector();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Director, meta = (ClampMin = 0.1f))
+	float SecondPerSpawn = 15.0f;
+
+	UPROPERTY(EditInstanceOnly, BlueprintInternalUseOnly, Category = Director)
+	TArray<AActor*> SpawnPoints;
+
+	UPROPERTY(EditInstanceOnly, BlueprintInternalUseOnly, Category = Director)
+	TArray<TSubclassOf<APickup>> PickupBPs;
+	
+	void SpawnTick();
+	static void FreePickup(FSpawnKey Key);
+
+	void SpawnPickup(int pickupIndex, int spawnPointIndex);
+
+	void SetFull(bool isFull);
+
+	void UpdateFrequencies(class ADeathMatchGS* GameState);
+};
